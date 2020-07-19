8856db518a81452aaebd8bf366f9ee81772c4c66
Sichang Li <sichang_li@apple.com>
2020-07-18 21:52:16 -0700

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/conFusion"
+  mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "888122208365687",
+    clientSecret: "20d8e0f0c47eaa4e0594e505dfdf0fe0"
+  }
 };
